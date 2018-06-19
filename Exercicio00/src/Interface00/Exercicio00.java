620ef2245988c6da4a5ea071fe5381a5d511e429
Guilherme Bialas <38558633+GuilhermeBialas@users.noreply.github.com>
2018-06-19 11:10:50 -0300

    continuação  do Exercicio00

--- Exercicio00/src/Interface00/Exercicio00.java
+++ Exercicio00/src/Interface00/Exercicio00.java
@@ -10,58 +10,79 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
  *
  * @author Guilherme Bialas
  */
-public class Exercicio00 implements JFrameBaseInterfaceJava{
+public class Exercicio00 implements JFrameBaseInterfaceJava {
+
     private JFrame jFrame;
-    private JTextField jTextField;
+    private JTextField jTextFieldNome;
     private JButton jButtonCancelar;
     private JButton jButtonSalvar;
-    private JCheckBox jCheckBox;
-
-    
+    private JCheckBox jCheckBoxRaca;
+    private JTextArea jTextAreaNome;
    
+    
+    
+    public Exercicio00() {
+        gerarTela();
+        instanciarComponentes();
+        gerarComponentes();
+        gerarDimensoes();
+        gerarLocalizacoes();
+        adicionarComponentes();
+        jFrame.setVisible(true);
+    }
+
     @Override
     public void gerarComponentes() {
-        
+
     }
 
     @Override
     public void instanciarComponentes() {
-     jButtonSalvar = new JButton("Salvar");
-     jButtonCancelar = new JButton("Cancelar");
+        jButtonSalvar = new JButton("Salvar");
+        jButtonCancelar = new JButton("Cancelar");
+        jTextFieldNome = new JTextField("Nome");
+        jCheckBoxRaca = new JCheckBox("Raça");
+        jTextAreaNome = new  JTextArea("");
     }
 
     @Override
     public void gerarDimensoes() {
-     
+     jButtonSalvar.setSize(100, 20);
+     jButtonCancelar.setSize(100, 20);
+     jTextFieldNome.setSize(55, 60);
     }
 
     @Override
     public void gerarLocalizacoes() {
-        
+        jButtonSalvar.setLocation(50, 50);
+        jButtonCancelar.setLocation( 60, 60);
+        jTextFieldNome.setLocation(10 , 10);
+        jTextAreaNome.setLocation(15, 20);
     }
 
     @Override
     public void adicionarComponentes() {
-       jFrame.add(jButtonSalvar);
-       jFrame.add(jButtonCancelar);
-       jFrame.add(jTextField);
-       jFrame.add(jCheckBox);
+        jFrame.add(jButtonSalvar);
+        jFrame.add(jButtonCancelar);
+        jFrame.add(jTextFieldNome);
+        jFrame.add(jCheckBoxRaca);
     }
 
     @Override
     public void gerarTela() {
         jFrame = new JFrame();
-        jFrame.setSize(618,423);
+        jFrame.setSize(600, 340);
         jFrame.setLayout(null);
         jFrame.setLocationRelativeTo(null);
         jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
+
     }
-    
+
 }
---
+++ Exercicio00/src/Interface00/Exercicio00Principal.java
@@ -0,0 +1,24 @@
+
+package Interface00;
+
+import java.awt.EventQueue;
+
+/**
+ *
+ * @author Guilherme Bialas
+ */
+public class Exercicio00Principal {
+
+   
+    public static void main(String[] args) {
+       EventQueue.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new Exercicio00();
+
+            }
+        });
+    }
+    
+}
